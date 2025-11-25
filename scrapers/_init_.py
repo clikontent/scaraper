6aeb9e3c1a13e4c3d307c209f5aa19b20cf2ce30
Oscar <oscarsmillie@gmail.com>
2025-11-25 14:22:10 +0300

    update something

--- scrapers/_init_.py
+++ scrapers/_init_.py
@@ -1,15 +1,10 @@
 from scrapers.remote_africa import scrape_remote_africa
-from scrapers.myjobmag import scrape_myjobmag
 from scrapers.remoteok import scrape_remoteok
 from scrapers.weworkremotely import scrape_weworkremotely
-from scrapers.brightermonday import scrape_brightermonday
-from scrapers.beyondthesavannah import scrape_beyondthesavannah
+
 
 SCRAPERS = [
     scrape_remote_africa,
-    scrape_myjobmag,
     scrape_remoteok,
     scrape_weworkremotely,
-    scrape_brightermonday,
-    scrape_beyondthesavannah
-]
+    ]
